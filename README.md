97156205a3915b5ada28be1fad6cfd691c41ba0d
mitsiu <mitsiu.carreno@gmail.com>
2020-08-10 00:54:31 -0500

    add comments on wxWidgets demo and wxMathPlot samples fix namespace error on wxMathPlot.cpp (::wxLogError -> wxLogErrorro)r)

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ nc -z -v -u 127.0.0.1 8080
 
 # Install wxWidgets
 brew install wxmac
+
+g++ -o2 -o wxMathPlot/sample1 wxMathPlot/sample1.cpp wxMathPlot/mathplot.cpp `wx-config --cxxflags --libs` && ./wxMathPlot/sample1
--- gui/gui.cpp
+++ gui/gui.cpp
@@ -4,11 +4,23 @@
 #ifndef WX_PRECOMP
     #include "wx/wx.h"
 #endif
+
+// Every app should define a new class derivated from wxApp (By overridding 
+// xnApp's Oninit() virtual method the program can be initialized)
 class MyApp : public wxApp
 {
 public:
     virtual bool OnInit();
 };
+
+// The main window is created by deriving a class from wxFrame and giving it a menu and status bar in its constructor
+// Also any clases that wishes to respond to an "event" (mouse clicks, messages from the menu or a button) must 
+// declare an event table using the macro below
+// Reacting to such event is done via "event handlers" (functions, functors lambdas...) taking the event parameter
+// of the type corresponding to the event being handled
+
+// In our example we react to three menu items: "Hello" "Exit" and "About" (any program should normally implement the latter two)
+// Notice these handlers don't need to ve virtual or public
 class MyFrame : public wxFrame
 {
 public:
@@ -18,17 +30,38 @@ private:
     void OnExit(wxCommandEvent& event);
     void OnAbout(wxCommandEvent& event);
 };
+
+// In order to be able to react to a menu command, it must be given a unique identifier (const variable or enum element) 
 enum
 {
     ID_Hello = 1
 };
+// Notice you don't need to define identifiers FOR "About" and "Exit" as wxWidgets already predefines standard values
+// such as wxID_ABOUT and wxID_EXIT ((use these whenever possible as they can be handled in a special way by a particular platform)
+
+// As in all programs, there must be a "main" function. Under wxWidgets, main is implemented inside the "wxIMPLEMENT_APP()" macro, 
+// which creates an application instance of the specified class and starts running the GUI event loop
 wxIMPLEMENT_APP(MyApp);
+
+// As mentioned wxApp::OnInit() is called upon startup and should be used to initialize the program (maybe showing a "splash screen") and creating the main window
+// Frames are created hidden by default, to allow the creation of child windows before displaying them
+// We need to explicitly show them
+// Finally we return true from this method to indicate successful initialization 
 bool MyApp::OnInit()
 {
     MyFrame *frame = new MyFrame();
     frame->Show(true);
     return true;
 }
+
+// In the constructor of the main window, we create a menu with our menu items, as well as a status bar to be shown at the bottom of the main window
+// Both have to be bound to the frame with respective calls
+
+// Notice we don't need to specify the labels for the standard menu items wxID_ABOUT and wxID_EXIT - they will be given standard labels
+// and standard accelerators correct for the current platform, making our program behave more native
+
+// We also have to connect our event handlers to the events we want to handle in them. We do this by calling Bind() to send all the menus events 
+// (identified by wxEVENT_MENU event type) with the specified ID to the given function
 MyFrame::MyFrame()
     : wxFrame(NULL, wxID_ANY, "Hello World")
 {
@@ -45,19 +78,33 @@ MyFrame::MyFrame()
     SetMenuBar( menuBar );
     CreateStatusBar();
     SetStatusText("Welcome to wxWidgets!");
+
+    // The params are:
+    // 1-The event type (wxEVT_MENU, wxEVT_BUTTON, wxEVT_SIZE...)
+    // 2-A pointer to the method call, and the object to call it on. In this case we just call our own function, and pass the "this" pointer for the object itself. 
+    // (In fact any object that can be called with a wxCommandEvent, can be used here)
+    // 3-An optional identifier, allowing us to select just some events of wxEVT_MENU type
     Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
     Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
     Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
 }
+
+// Standard event handler implementations
+// MyFrame::OnExit() closes the main window by calling Close(). The parameter true indicates that other windows have no veto power such as after asking "Do you really want to close?" 
+// If there is no other main window left, the application will quit
 void MyFrame::OnExit(wxCommandEvent& event)
 {
     Close(true);
 }
+
+// MyFrame::OnAbout will display a small window with some text in it. In this case a typical "About" window
 void MyFrame::OnAbout(wxCommandEvent& event)
 {
     wxMessageBox("This is a wxWidgets Hello World example",
                  "About Hello World", wxOK | wxICON_INFORMATION);
 }
+
+// The implementation of custom menu command handler may perform whatever task you program needs to do, in these case we will simply show a message
 void MyFrame::OnHello(wxCommandEvent& event)
 {
     wxLogMessage("Hello world from wxWidgets!");
