b9c55a102f0d7f0d699ab0968fe4516053f924b9
mitsiu-carreno <mitsiu.carreno@gmail.com>
2022-09-21 14:49:38 -0500

    testing websockets

---
+++ plot/v2/index.js
@@ -0,0 +1,49 @@
+/*
+const fs = require('fs');
+const es = require('event-stream');
+
+
+var s = fs.createReadStream('../../test.csv')
+  .pipe(es.split())
+  .pipe(es.mapSync((line)=>{
+      s.pause();
+      io.emit('data', line);
+
+      s.resume(); 
+    })
+    .on('error', (err)=>{
+      console.log("Error while reading file", err);
+    })
+    .on('end', ()=>{
+      console.log("Read entire file");
+    })
+  );
+  *
+
+*/
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log("connect");
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', ()=>{
+    console.log("disconnect"); 
+  });
+});
+
+
+server.listen(3000, () => {
+  console.log('listening on port 3000');
+});
